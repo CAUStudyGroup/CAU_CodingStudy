ec573898adb4e1c23f1e1baabf58a52db36b6835
jiho <hoye2210@naver.com>
2019-04-05 21:07:28 +0900

    solved

--- Codeup_1430_JIHO.cpp
+++ Codeup_1430_JIHO.cpp
@@ -1,42 +1,33 @@
 #include <iostream>
-
 using namespace std;
 
-long int arr_N[10000001];
+int arr[10000001];
 
 int main()
 {
 	long int N;
+	int tmp = 0;
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
-		cin >> arr_N[i];
+		cin >> tmp;
+		arr[tmp - 1] = tmp;
 	}
-	int arr_M[100000];
 
-	int M; 
+	int arr_M[100000] = { 0 };
+	int M;
 	cin >> M;
 
 	for (int i = 0; i < M; i++)
-	{
 		cin >> arr_M[i];
-	}
 
 	for (int i = 0; i < M; i++)
 	{
-		for (int j = 0; j < N; j++)
-		{
-			if (arr_M[i] == arr_N[j])
-			{
-				cout << "1 ";
-				arr_M[i] = 0;
-				break;
-			}
-		}
-		if (arr_M[i] != 0)
+		if (arr_M[i] == arr[arr_M[i] - 1])
+			cout << "1 ";
+		else
 			cout << "0 ";
 	}
-	
 
 	return 0;
 }
