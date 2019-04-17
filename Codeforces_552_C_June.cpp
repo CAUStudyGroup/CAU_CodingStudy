26cd49a63eefbff8ed73ef6cd99ed2d75aa807f9
June <rlawns3244@naver.com>
2019-04-18 00:10:46 +0900

    TimeExd on test5 CF552C

--- Codeforces_552_C_June.cpp
+++ Codeforces_552_C_June.cpp
@@ -1,44 +1,79 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define SWAP(a, b)((a) != (b) ? ((a) ^= (b), (b) ^= (a), (a) ^= (b)) : ((a), (b)))
-#define MAX(a, b)((a) > (b) ? (a) : (b))
 using namespace std;
+enum DAYS{
+	MON,TUE,WED,THU,FRI,SAT,SUN
+};
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	long long foods[3];
-	long long dayCnt = 0;
-	vector<long long>v_dayCnt;
+	int foods[3];
+	int tmpStoredFoods[3];
+	int today;
+	int dayCnt = 0;
+	int itDay;
+	int firstDay, lastDay;
+	int maxFood=-1, maxIdx;
+	bool isTripEnd = false;
+	vector<int>v_dayCnt;
 	for (int i = 0; i < 3; i++) {
 		cin >> foods[i];
-	}
-	for (int j = 0; j < 7; j++) {
-		for (int i = j; i < 7; i++) {
-			dayCnt++;
-			switch (i)
-			{
-			case 0: case 3: case 6:
-				foods[0]--;
-				break;
-			case 1: case 5:
-				foods[1]--;
-				break;
-			case 2: case 4:
-				foods[2]--;
-				break;
-			}
-			if (foods[0] == 0 || foods[1] == 0 || foods[2] == 0) {
-				break;
-			}
-			else if(i == 6){
-				i = j;
-			}
+		tmpStoredFoods[i] = foods[i];
+		if (maxFood <= foods[i]) {
+			maxFood = foods[i];
+			maxIdx = i;
 		}
+	}
+	switch (maxIdx)
+	{
+	case 0://Fish
+		firstDay = FRI;
+		lastDay = SUN;
+		break;
+	case 1://Rabbit
+		firstDay = MON;
+		lastDay = WED;
+		break;
+	case 2://Chicken
+		firstDay = TUE;
+		lastDay = THU;
+		break;
+	}
+	for (itDay = firstDay; itDay <= lastDay; itDay++) {
+		isTripEnd = false;
+		dayCnt = itDay;
+		while (true) {
+			today = dayCnt % 7;
+			switch (today)
+			{
+			case MON: case THU: case SUN:
+				if (0 != foods[0])
+					foods[0]--;
+				else
+					isTripEnd = true;
+				break;
+			case TUE: case SAT:
+				if (0 != foods[1])
+					foods[1]--;
+				else
+					isTripEnd = true;
+				break;
+			case WED: case FRI:
+				if (0 != foods[2])
+					foods[2]--;
+				else
+					isTripEnd = true;
+				break;
+			}
+			if (isTripEnd) break;
+			else dayCnt++;
+		}
+		dayCnt -= itDay;
 		v_dayCnt.push_back(dayCnt);
-		dayCnt = 0;
+		std::copy(std::begin(tmpStoredFoods), std::end(tmpStoredFoods), std::begin(foods));
 	}
 	sort(v_dayCnt.begin(), v_dayCnt.end());
-	cout << v_dayCnt[v_dayCnt.size()-1];
+	cout << v_dayCnt[v_dayCnt.size() - 1];
 	return 0;
-} //Solving......Time limit exceeded on test 1
+}
